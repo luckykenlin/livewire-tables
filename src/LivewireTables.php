c370bc5d7bc7de88d9819bdf1ba28a717bd6dac0
luckykenlin <luckykenlin@icloud.com>
2021-03-25 18:41:04 -0400

    init: setup test env

--- src/LivewireTables.php
+++ src/LivewireTables.php
@@ -2,6 +2,62 @@
 
 namespace Luckykenlin\LivewireTables;
 
-class LivewireTables
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Arr;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+abstract class LivewireTables extends Component
 {
+    use WithPagination;
+
+    protected Builder $query;
+
+    public function __construct($id = null)
+    {
+        parent::__construct($id);
+        $this->query = $this->initialQuery();
+    }
+
+    abstract protected function query(): Builder;
+
+    abstract protected function columns(): array;
+
+    protected function view(): string
+    {
+        return 'livewire.table';
+    }
+
+    public function render(): View
+    {
+        return view($this->view(), [
+            'columns' => $this->columns(),
+            'rows' => $this->models()
+        ]);
+    }
+
+    protected function models()
+    {
+        return $this->query->paginate($this->perPage);
+    }
+
+    public function columnValue($row, $column)
+    {
+        if ($column->isFormatted()) {
+            return app()->call($column->formatCallback, ['value' => Arr::get($row->toArray(), $column->attribute)]);
+        }
+
+        return Arr::get($row->toArray(), $column->attribute);
+    }
+
+    /**
+     * Initial query and avoid sql ambiguous column name via join connection.
+     *
+     * @return Builder
+     */
+    private function initialQuery()
+    {
+        return $this->query()->select("{$this->getTable($this->query())}.*");
+    }
 }
