2961047eccdce1adbeec8031059e9d8d39d7b1fc
luckykenlin <luckykenlin@icloud.com>
2021-04-07 03:10:16 -0400

    feat: add table stub and make command.

--- config/livewire-tables.php
+++ config/livewire-tables.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-
+    "theme_template" => "tailwind"
 ];
--- src/Commands/LivewireTablesCommand.php
+++ src/Commands/LivewireTablesCommand.php
@@ -3,15 +3,26 @@
 namespace Luckykenlin\LivewireTables\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 
 class LivewireTablesCommand extends Command
 {
-    public $signature = 'livewire-tables';
+    public $signature = 'make:table {name} {--model=Model}';
 
-    public $description = 'My command';
+    public $description = 'Make livewire table.';
 
     public function handle()
     {
-        $this->comment('All done');
+        $stub = File::get(__DIR__ . '/../../resources/stubs/component.stub');
+        $stub = str_replace('DummyTable', $this->argument('name'), $stub);
+        $stub = str_replace('DummyModel', $this->option('model'), $stub);
+        $path = app_path('Http/Livewire/' . $this->argument('name') . '.php');
+
+        File::ensureDirectoryExists(app_path('Http/Livewire'));
+
+        if (!File::exists($path) || $this->confirm($this->argument('name') . ' already exists. Overwrite it?')) {
+            File::put($path, $stub);
+            $this->info($this->argument('name') . ' was made!');
+        }
     }
 }
