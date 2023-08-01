32c8d626ed2cf109c58c750b1e7c0117d8bfbf90
ken lin <kenl@goetu.com>
2023-08-01 12:28:42 -0400

    bugfix: undfined array key

--- src/Traits/Filterable.php
+++ src/Traits/Filterable.php
@@ -108,7 +108,7 @@ trait Filterable
      */
     public function removeFilter($uriKey): void
     {
-        if (! isset($this->filters[$uriKey])) {
+        if (blank(data_get($this->filters, $uriKey))) {
             return;
         }
 
@@ -123,7 +123,7 @@ trait Filterable
     public function countFilters(): int
     {
         return collect($this->filters)
-            ->reject(fn ($value) => $value === '')
+            ->reject(fn($value) => $value === '')
             ->count();
     }
 
